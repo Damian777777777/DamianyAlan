b88bebd54b85033c67c90732369b7c1ca185a6bc
Damian Aguilera <damiantego@gmail.com>
2025-04-04 20:12:26 -0600

    Subiendo front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,6 @@ import Signup from "./pages/Signup";
 import About from "./pages/About";
 import UserManagement from "./pages/UserManagement"; // Import UserManagement page
 import ClarityDashboard from "./pages/ClairityDashboard";
-import DashboardDetail from "./pages/DashboardDetail";
-import DeviceManagement from "./pages/DeviceMonitoring";
 import Profile from "./pages/Profile"; // Import Profile page
 
 const REACT_APP_BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
@@ -21,9 +19,6 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/users" element={<UserManagement />} /> {/* New route for user management */}
         <Route path="/dashboard" element={<ClarityDashboard />} />
-        <Route path="/detail" element={<DashboardDetail />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/devices" element={<DeviceManagement />} />
         <Route path="/profile" element={<Profile />} /> {/* New route for profile */}
       </Routes>
     </Router>
